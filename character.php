904d5082a912923959fb2dda6e2acad36e3f6652
Dirkhoogland <99063413@mydavinci.nl>
2021-03-19 11:04:43 +0100

    include en require

---
+++ character.php
@@ -0,0 +1,19 @@
+<?php
+ require 'index.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>character</title>
+</head>
+<body>
+
+
+
+
+
+<p id="footer">dirk hoogland 2021</p>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<<?php 
+
+include_once 'character.php';
+
+
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>index</title>
+ </head>
+ <body>
+ <p id="footer">dirk hoogland 2021</p>
+ </body>
+ </html>
